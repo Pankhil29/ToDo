b4d387a678825b632572e87dafeb6cd23df8ffb7
Pankhil29 <pankhilpatel734@gmail.com>
2025-11-26 11:03:19 +0530

    completed update and delete operations

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Task
 from django.shortcuts import redirect
 
@@ -8,3 +8,41 @@ def addTask(req):
     task = req.POST['task']
     Task.objects.create(task=task)
     return redirect("home")
+
+def mark_as_done(req,pk):
+    task = get_object_or_404(Task,pk=pk)
+    # for finding a particular object based on primary key
+    task.is_completed = True
+    task.save()
+    return redirect("home")
+
+def mark_as_undone(req,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+def edit_task(req,pk):
+    get_task = get_object_or_404(Task,pk=pk)
+    if req.method == "POST":   
+        new_task = req.POST['task']    # it give dictionary of data
+        get_task.task = new_task
+        get_task.save()
+        return redirect("home")
+    else:
+        context = {
+            'get_task':get_task
+        }
+    return render(req,'edit_task.html',context)
+
+def delete_task(req,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
+    
+    
+# url -> views -> model -> database
+# in html file we will call the url in buttons 
+
+# get -> to fetch data from server or load the data
+# post -> to send data to the server or updating data
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-update_at') # for - is descending order
+    completed_tasks = Task.objects.filter(is_completed=True)
+    # is_completed
     context = {
         'tasks' : tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, "home.html",context)
